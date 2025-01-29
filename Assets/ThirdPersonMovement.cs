3dd2f7ee28dee0203172faf8920bff12434924c9
Beel550216 <550216@chrome.escg.ac.uk>
2025-01-29 10:37:30 +0000

    Initial commit

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    void Update()
+    {
+        float horizontal = Input
+    }
+}
